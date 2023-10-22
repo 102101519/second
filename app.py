256f64c7318ee7a37f82df7f95162f168d2029f4
102101519 <146419672+102101519@users.noreply.github.com>
2023-10-22 16:33:57 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('/static/index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    try:
+        expression = request.form['expression']
+        result = eval(expression)
+        insertResult(expression, result)
+        return jsonify({"result": result})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
